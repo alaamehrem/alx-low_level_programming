ca04bc134086855329ec56b9a605829dbf15a7d4
alaamehrem <alaammehrem@gmail.com>
2023-04-21 19:18:43 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 57; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
-		putchar(',');
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar(57);
 	putchar('\n');
 	return (0);
 }
