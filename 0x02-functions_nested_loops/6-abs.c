a861b16f658e8a7f2100cda6e92f129767a03112
alaamehrem <alaammehrem@gmail.com>
2023-04-25 19:30:53 -0700

    int _abs(int)

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - prints absolute of a number
+ * @n: the nb checked
+ * Return: always 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+		return (n);
+}
