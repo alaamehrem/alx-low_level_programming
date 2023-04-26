22f6a5cdb1dca660c794a7d16215bd65c382e97e
alaamehrem <alaammehrem@gmail.com>
2023-04-25 18:14:04 -0700

    alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,12 +4,14 @@
  */
 void print_alphabet_x10(void)
 {
-	int i=0;
-	char ch='a';
+	int i = 0;
+	char ch;
 
 	while (i < 10)
 	{
-		while (ch <='z')
+		ch = 'a'
+
+		while (ch <= 'z')
 		{
 			_putchar(ch);
 			ch++;
