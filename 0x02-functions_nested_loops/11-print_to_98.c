efad8dd572265f78bccf305186a04c3bf567d638
alaamehrem <alaammehrem@gmail.com>
2023-04-27 04:53:29 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,14 +21,8 @@ void print_to_98(int n)
 	}
 	for (i = n; i >= 98; i--)
 	{
-		if (i == 98)
-		{
-			printf("%d", i);
-		}
-		else
-		{
-			printf("%d, ", i);
-		}
+		printf("%d, ", i);
+		printf("98");
 	}
 	printf("\n");
 }
