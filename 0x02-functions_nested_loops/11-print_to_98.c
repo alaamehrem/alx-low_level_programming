57ff74becb3e2a200e5a98f8f2a9b3cdff11be71
alaamehrem <alaammehrem@gmail.com>
2023-04-27 04:18:18 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,13 @@
 #include "main.h"
 /**
  * print_to_98 - zeft
- * @n: the nember
+ * @n: the number
+ * @i: the counter
  */
 void print_to_98(int n)
 {
+	int i;
+
 	for (i = n; i <= 98; i++)
 	{
 		if (i == 98)
