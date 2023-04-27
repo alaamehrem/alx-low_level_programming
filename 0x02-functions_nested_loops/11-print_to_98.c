4c5208babaa5e2526571708f688096500ee08adc
alaamehrem <alaammehrem@gmail.com>
2023-04-27 02:59:46 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_to_98 - zeft
+ * @n: the nember
+ */
+void print_to_98(int n)
+{
+	for (i = n; i <= 98; i++)
+	{
+		if (i == 98)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf("%d,  ", i);
+		}
+	}
+}
