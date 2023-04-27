5217e41be92b4ec05372e335ad75c3db60d12c31
alaamehrem <alaammehrem@gmail.com>
2023-04-27 02:22:26 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,41 +5,39 @@
  */
 void times_table(void)
 {
-    int i , j ;
-    for (i=0 ; i < 10 ; i++)
-    {
-        for(j = 0 ; j < 10 ; j++)
-        {
-        int r;
+	int i, j;
 
-        r = i*j;
-	if (j == 0 && r < 10)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(r + '0');
+		for (j = 0; j < 10; j++)
+		{
+			int r;
+
+			r = i * j;
+		}
+		if (j == 0 && r < 10)
+		{
+			_putchar(r + '0');
+		}
+		else if (j == 0 && r > 10)
+		{
+			_putchar((r / 10) + '0');
+			_putchar((r % 10) + '0');
+		}
+		else if (r < 10 && j != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(r + '0');
+		}
+		else
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar((r / 10) + '0');
+			_putchar((r % 10) + '0');
+		}
+	_putchar('\n');
 	}
-	else if (j==0 && r >10)
-	{
-		_putchar((r/10) + '0');
-		_putchar((r%10) + '0');
-	}
-	else if (r < 10 && j != 0)
-        {
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(r + '0');
-        }
-        else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar((r/10) + '0');
-		_putchar((r%10) + '0');
-        }
-        }
-    _putchar('\n');
-    }
 }
-
-	
-
