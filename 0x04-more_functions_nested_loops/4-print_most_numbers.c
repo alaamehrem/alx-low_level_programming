17ee7b3ce198b6582650ce6be2f99d000cdd838f
alaamehrem <alaammehrem@gmail.com>
2023-04-27 18:15:15 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,7 +8,9 @@ void print_most_numbers(void)
 
 	for ( i = 48 ; i < 58 ; i++)
 	{
-		if (!( i = 50 || i = 54))
+		if (i == 50 || i == 54)
+			continue;
+		else
 			_putchar(i);
 	}
 	_putchar('\n');
