c11471ffb0f47250c841b36faff73c497b1d2752
alaamehrem <alaammehrem@gmail.com>
2023-04-27 18:10:58 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
 
 void print_numbers(void)
 {
-	char i;
+	char c;
 
-	for (i = 48; i < 58; i++)
+	for (c = 48; c < 58; c++)
 	{
-		_putchar(i);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints number other than 2 and 4
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for ( i = 48 ; i < 58 ; i++)
+	{
+		if ( i = 50 || i = 54)
+			continue;
+		else
+			_putchar(i);
+	}
+	_putchar('\n');
+}
