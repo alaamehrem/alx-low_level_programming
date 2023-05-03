af491e75048e5e77236d0bee583b72d09fdb0177
alaamehrem <alaammehrem@gmail.com>
2023-05-02 18:22:24 -0700

    void puts2(char *str)

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - puts string
+ * @str: value
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		int i = 0;
+
+		if (i % 2 = 0)
+			_putchar(*str);
+		else
+			continue;
+		str++;
+		i++;
+	}
+	_putchar('\n');
+}
