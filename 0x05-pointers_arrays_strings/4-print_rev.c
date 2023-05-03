c1b49f4fcb50680ca9e04330a6e6ba49f0c1a4ca
alaamehrem <alaammehrem@gmail.com>
2023-05-02 17:54:05 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_rev - prints a string, in reverse, followed by a new line
+ * _strlen - returns the length of a string
  * @s: string
+ * Return: counter value
  */
 void print_rev(char *s)
 {
+	int i = 0;
+
 	while (*s != '\0')
+	{
+		i++;
 		s++;
+	}
+	s--;
 	while (*s >= 0)
 	{
 		_putchar(*s);
 		s--;
 	}
 	_putchar('\n');
+
 }
