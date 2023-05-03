f95dc93fdf287c44d53b4fdc75eb1d8baa90a09c
alaamehrem <alaammehrem@gmail.com>
2023-05-02 17:17:40 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,12 +3,14 @@
 /**
  * _strlen - returns the length of a string
  * @s: string
+ * @i: counter
+ * Return: counter value
  */
 int _strlen(char *s)
 {
 	int i = 0;
 
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		i++;
 		s++;
