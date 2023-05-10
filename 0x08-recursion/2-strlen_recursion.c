5e42cf304d8a36ed2fc5d770c11afa6950de9caf
alaamehrem <alaammehrem@gmail.com>
2023-05-10 12:45:17 -0700

    c recursions 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,10 +6,12 @@
 int i = 0;
 int _strlen_recursion(char *s)
 {
-	if(*s != '\0')
+	int i = 0;
+
+	if (*s != '\0')
 	{
 		i++;
-		_strlen_recursion(s + 1);
+		i += _strlen_recursion(s + 1);
 	}
 	return (i);
 }
