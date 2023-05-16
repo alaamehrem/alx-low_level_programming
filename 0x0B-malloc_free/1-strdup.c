e3bc58c5199ab8f426e855eeac3282f978011bed
alaamehrem <alaammehrem@gmail.com>
2023-05-16 13:38:36 -0700

    c mallocs

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	char *ptr;
+
+	while (str[i] != '\0')
+		i++;
+	ptr = malloc(sizeof(char) * (i + 1));
+	if (ptr == NULL || str == NULL)
+		return (NULL);
+	while (str[j] != '\0')
+	{
+		ptr[k] = str[j];
+		k++;
+		j++;
+	}
+	return (ptr);
+}
