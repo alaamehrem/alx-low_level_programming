a22c50587e94daeaff0bb19dafd9104faf7ddee7
alaamehrem <alaammehrem@gmail.com>
2023-05-22 15:07:09 -0700

    C structs and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog's basic info
+ * @name: First dog
+ * @age: Second dog
+ * @owner: Third dog
+ * Description: dog longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
